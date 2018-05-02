4a3a9677acc78f317062a9dc9edc8f922217d087
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2018-05-02 09:46:35 -0400

    Add all basic functionality for Model.m

--- Model.m
+++ Model.m
@@ -1,22 +1,29 @@
-classdef Model
+classdef Model < handle
     properties
+        inputs
+        outputs
         weights
         biases
         activation_functions
+        feature_ranges
         label_ranges
     end
     methods
-        function obj = Model(weights, biases, activation_functions)
+        function obj = Model(inputs, outputs, weights, biases, activation_functions, feature_ranges, label_ranges)
+            obj.inputs = inputs;
+            obj.outputs = outputs;
             obj.weights = weights;
             obj.biases = biases;
             obj.activation_functions = activation_functions;
+            obj.feature_ranges = feature_ranges;
+            obj.label_ranges = label_ranges;
         end
 
         function y = infer(obj, features)
-            y = features;
+            y = obj.scale(features);
             for n = 1:length(obj.weights)
                 y = y*obj.weights{n} + obj.biases{n};
-                y = obj.ACT(y, obj.activation_functions(n + 1))
+                y = obj.ACT(y, obj.activation_functions{n + 1});
             end
             y = obj.descale(y);
         end
@@ -32,6 +39,10 @@ classdef Model
                 case 'none'
                     y = x;
             end
+        end
+
+        function y = scale(obj, x)
+            y = (x - obj.feature_ranges(1, :))./(obj.feature_ranges(2, :) - obj.feature_ranges(1, :));
         end
 
         function y = descale(obj, x)
